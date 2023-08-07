72922ff5c6b1a9d98f49ae67da26ae4de590cfe2
Akanksha Priya <112411640+akanksha23-priya@users.noreply.github.com>
2023-08-07 14:40:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// const rangeEl =document.querySelector('input')
+// const percentage = document.querySelector('.percentage')
+// const percentageSpan = document.querySelector('.percentage span')
+
+// rangeEl.addEventListener('input',range)
+
+// function range () {
+//     percentage.style.left = `${rangeEl.value}%`;
+//     percentageSpan.innerText = `${rangeEl.value}%`;
+//     // percentage.style.filter= `${rangeEl.value}%`;
+// }
